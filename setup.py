03eb737b02d39ffdb12a637bdc41f18b1462d0a0
medjedqt <godtier33@gmail.com>
2020-10-29 11:19:54 +0800

    add README and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="qpy",
+	version="0.0.1",
+	author="medjed",
+	author_email="imoshugi01@gmail.com",
+	description="A basic qpost API wrapper",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/medjedqt/qpost",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3",
+		"Topic :: Internet",
+		"Topic :: Software Development :: Libraries",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Utilities",
+	],
+	python_requires=">=3.6"
+)
