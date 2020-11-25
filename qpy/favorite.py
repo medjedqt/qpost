55d912834515a34733da33fe223daac2f701e0fc
medjedqt <godtier33@gmail.com>
2020-11-25 20:38:25 +0800

    missing typehint

--- qpy/favorite.py
+++ qpy/favorite.py
@@ -6,7 +6,7 @@ class Favorite:
 	def __init__(self, favdict, bot):
 		self.id: int = favdict['id'] #: The unique identifier of this Favorite
 		self.user: User = User(favdict['user'], bot) #: The creator of this Favorite
-		self.entry = FeedEntry(favdict['feedEntry'], bot) #: The FeedEntry that was favorited
+		self.entry: FeedEntry = FeedEntry(favdict['feedEntry'], bot) #: The FeedEntry that was favorited
 		self.time_created: str = favdict['time'] #: The timestamp of when this Favorite was created
 		self.bot = bot
 	
